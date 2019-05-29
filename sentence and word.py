ec450e088268e01e4b65e2a2dfc8b22caf4fffaa
Mins97 <tkarnrwl7862@naver,com>
2019-05-30 07:54:01 +0900

    first commit

---
+++ sentence and word.py
@@ -0,0 +1,8 @@
+word = 'word'
+sentence = "This is a sentence"
+paragraph = """This is a paragraph. It is \
+mad up of multiple lines and setences."""
+
+print(word)
+print(sentence)
+print(paragraph)
